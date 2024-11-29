d0b87d69a8b103889113fbaef8a9b73f6243b222
sergiocoba <146813201+sergiocoba@users.noreply.github.com>
2024-11-29 17:09:49 +0100

    ddqdwdwd

--- LudoPingPing/Ball.gd
+++ LudoPingPing/Ball.gd
@@ -7,6 +7,8 @@ func _draw():
 var velocidad = 500
 var direccion = 1
 var reset = false
+var sonido_reproducido = false
+
 
 func _ready():
 	linear_velocity.x = velocidad*direccion
@@ -17,10 +19,23 @@ func _physics_process(delta:float):
 
 func _on_Ball_body_entered(body):
 	if body.is_in_group("Player"):
+		reproducir_sonido()
 		print("entro")
 		direccion*=-1
-	pass
+	else:
+		$AudioStreamPlayer.stop();
+		
 
+func reproducir_sonido():
+	if $AudioStreamPlayer.stream and not $AudioStreamPlayer.is_playing():
+			$AudioStreamPlayer.play();
+			sonido_reproducido = true
+			
+		
+func _physics_process_post(delta):
+	# Restablecer la bandera cuando ya no haya colisiones con la bola
+		sonido_reproducido = false
+		
 func _integrate_forces(state: Physics2DDirectBodyState):
 	if reset:
 		direccion*=-1
--- LudoPingPing/World.gd
+++ LudoPingPing/World.gd
@@ -4,9 +4,6 @@ var jugador1 = 0
 var jugador2 = 0
 
 
-
-
-	
 var bolas = {
 	"Porti": preload("res://bolas/porti.png"),
 	"Josep": preload("res://bolas/josep.png"),
@@ -19,21 +16,30 @@ func _ready():
 	var Bola = get_node("/root/World/Ball/ColisionBola/Bola")
 	Bola.texture = bolas[Globals.bola_seleccionada]
 
+
+
 func _on_Area2DDerecha_body_entered(body):
 	if body.is_in_group("Ball"):
-		jugador1 +=1;
-		reiniciarNivel()
-	pass # Replace with function body.
+		jugador1 += 1
 
+
+		reiniciarNivel()
 
 func _on_Area2DIzquierda_body_entered(body):
 	if body.is_in_group("Ball"):
-		jugador2 +=1;
+		jugador2 += 1
+
+
 		reiniciarNivel()
-	pass # Replace with function body.
 
 func reiniciarNivel():
-	$ScoreTextoDerecha.text=str(jugador2)
-	$ScoreTextoIzquierda.text=str(jugador1)
+	# Actualizar el marcador
+	$ScoreTextoDerecha.text = str(jugador2)
+	$ScoreTextoIzquierda.text = str(jugador1)
 	$Ball.reset = true
-	
+
+	# Generar un color aleatorio
+	var random_color = Color(randi() % 256 / 255.0, randi() % 256 / 255.0, randi() % 256 / 255.0)
+
+	# Aplicar el color al fondo
+	self.modulate = random_color  # Cambia el color del nodo Node2D, que afecta a su fondo
